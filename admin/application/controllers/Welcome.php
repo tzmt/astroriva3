6351cc2842aff03ed1d2e929137165a8ede2e424
subhomoy <subhomoy.jobs@gmail.com>
2017-07-08 11:56:17 +0530

    commit

--- admin/application/controllers/Welcome.php
+++ admin/application/controllers/Welcome.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
+	/** 
 	 * Index Page for this controller.
 	 *
 	 * Maps to the following URL
