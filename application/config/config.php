c5398aad0ddc409dacf3c1711edf009cbaa3b9b6
subhomoy <subhomoy.jobs@gmail.com>
2017-08-01 22:42:23 +0530

    csrf

--- application/config/config.php
+++ application/config/config.php
@@ -443,7 +443,7 @@ $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
 $config['csrf_regenerate'] = TRUE;
-$config['csrf_exclude_uris'] = array('astrologyajax/getajaxrashi');
+$config['csrf_exclude_uris'] = array('astrologyajax/getajaxrashi','shop/updateProducts','shop/addReview/[0-9]/[a-z]');
 
 /*
 |--------------------------------------------------------------------------
